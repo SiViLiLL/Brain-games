547860b112748934c2ee1029a393239278c15f53
Dilyara Arslanova <risharsivil@gmail.com>
2023-01-23 11:40:34 +0300

    fix logic in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import readlineSync from 'readline-sync';
 import askUserName from './cli.js';
 
-const startGame = (game, gameInstructions) => {
+export default (gamesRound, gameInstructions) => {
   const userName = askUserName();
   console.log(gameInstructions);
   const numberOfRounds = 3;
   for (let i = 0; i < numberOfRounds; i += 1) {
-    game();
+    const userAnswer = '';
+    const roundResult = gamesRound(userName);
+    if (roundResult === 'wasted') {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, '${userName}'!`);
+      break;
+    } else if (roundResult === 'round passed') {
+      console.log('Correct!');
+    }
   }
   console.log(`Congratulations, ${userName}!`);
 };
